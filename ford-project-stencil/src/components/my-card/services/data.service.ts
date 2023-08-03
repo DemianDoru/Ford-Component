8437eda0627eb1eda44e677d65481d517313fa4a
Doru Demian <demian.doru@outlook.com>
2023-08-03 10:17:07 +0300

    second add

--- ford-project-stencil/src/components/my-card/services/data.service.ts
+++ ford-project-stencil/src/components/my-card/services/data.service.ts
@@ -1,7 +1,35 @@
+import { VehicleRecallDto } from '../models/vehicle-recall-dto.model';
+
 export class DataService {
   async fetchDataFromAPI(vin: string): Promise<any> {
     try {
       const apiUrl = 'http://oemapi-env.eba-g3qtdyqb.us-east-2.elasticbeanstalk.com/api/vindecoder';
+      const postData = {
+        vin: vin,
+      };
+
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error('Error making POST request:', error);
+      throw error;
+    }
+  }
+
+  async getVehicleRecalls(vin: string): Promise<VehicleRecallDto> {
+    try {
+      const apiUrl = 'http://oemapi-env.eba-g3qtdyqb.us-east-2.elasticbeanstalk.com/api/vindecoder/vehiclerecall';
       const postData = {
         vin: vin,
       };
--- ford-project-stencil/src/components/my-card/vin-decoder/vin-decoder.tsx
+++ ford-project-stencil/src/components/my-card/vin-decoder/vin-decoder.tsx
@@ -1,3 +1,5 @@
+// vin-decoder.tsx
+
 import { Component, h, Prop, State } from '@stencil/core';
 import { DataService } from '../services/data.service';
 import { VinData } from '../vin-decoder';
@@ -11,11 +13,13 @@ export class VinDecoder {
   @Prop() vin: string;
   @State() fetchedData: VinData[] = [];
   @State() vehicleDetails: any;
+  @State() vehicleRecalls: any;
   private dataService: DataService = new DataService();
 
   async componentWillLoad() {
     if (this.vin) {
       await this.fetchData();
+      await this.fetchRecalls();
     }
   }
 
@@ -24,7 +28,6 @@ export class VinDecoder {
       const response = await this.dataService.fetchDataFromAPI(this.vin);
       console.log('API Response:', response);
       const vehicleData = response?.decode?.vehicle[0];
-      const vehicleDetails = response?.decode?.vehicle[0]
       if (vehicleData && vehicleData.equip && vehicleData.equip.length > 0) {
         const fetchedData = vehicleData.equip;
         console.log('Fetched Data:', fetchedData);
@@ -33,9 +36,21 @@ export class VinDecoder {
         console.error('No valid data for vehicle equipment.');
         this.fetchedData = [];
       }
+      this.vehicleDetails = vehicleData;
     } catch (error) {
       console.error('Error fetching data:', error);
       this.fetchedData = [];
+    }
+  }
+
+  async fetchRecalls() {
+    try {
+      const response = await this.dataService.getVehicleRecalls(this.vin);
+      console.log('Recalls API Response:', response);
+      this.vehicleRecalls = response?.results;
+    } catch (error) {
+      console.error('Error fetching recalls:', error);
+      this.vehicleRecalls = [];
     }
   }
 
@@ -63,65 +78,32 @@ export class VinDecoder {
                 ))}
               </tbody>
             </table>
+          </div>
+        )}
+
+        {this.vehicleRecalls?.length > 0 && (
+          <div>
+            <h3>Recalls:</h3>
             <table>
               <thead>
                 <tr>
-                  <th>Detail component</th>
+                  <th>Component</th>
                   <th>Summary</th>
-                  <th>Conequence</th>
+                  <th>Consequence</th>
                 </tr>
               </thead>
               <tbody>
-                {this.vehicleDetails.results.map((detail: { component: any; summary: any; conequence: any; }) => (
-                  <div class="results">
-                    <div class="title">{detail.component}</div>
-                    <div class="content">{detail.summary}</div>
-                    <div class="content">{detail.conequence}</div>
-                  </div>
+                {this.vehicleRecalls.map((recall, index) => (
+                  <tr key={index}>
+                    <td>{recall.component}</td>
+                    <td>{recall.summary}</td>
+                    <td>{recall.consequence}</td>
+                  </tr>
                 ))}
               </tbody>
             </table>
           </div>
         )}
-
-        {/* <div class={this.isIos ? 'safeArea' : ''}> */}
-        <div class="container">
-          {/* <ion-icon onClick={() => this.backButton()} name="chevron-back-outline"></ion-icon> */}
-          {this.vehicleDetails && (
-            <ion-text color="light">
-              <h1>
-                {this.vehicleDetails.results[0].make}
-                {this.vehicleDetails.results[0].model}
-              </h1>
-            </ion-text>
-          )}
-          {this.vehicleDetails && (
-            <ion-text color="light">
-              <div class="details">{this.vehicleDetails.results[0].modelYear}</div>
-            </ion-text>
-          )}
-          {this.vehicleDetails && (
-            <ion-text color="light">
-              <div class="details">VIN: {this.vin}</div>
-            </ion-text>
-          )}
-          {this.vehicleDetails && (
-            <ion-text color="light">
-              <h4>Recalls</h4>
-            </ion-text>
-          )}
-          {this.vehicleDetails && (
-            <ion-list lines="none">
-              {this.vehicleDetails.results.map(detail => (
-                <div class="results">
-                  <div class="title">{detail.component}</div>
-                  <div class="content">{detail.summary}</div>
-                  <div class="content">{detail.conequence}</div>
-                </div>
-              ))}
-            </ion-list>
-          )}
-        </div>
       </div>
     );
   }
